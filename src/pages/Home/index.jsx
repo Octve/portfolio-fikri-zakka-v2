abd87e90c62d60767da556457b4438738380feaf
Octve <2010512048@mahasiswa.upnvj.ac.id>
2025-10-02 14:47:52 +0700

    feat: customized home section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,19 @@
 // import './index.stylesheet.css';
-import "./index.scss";
-import profileImage from "../../assets/images/home/profileImage.png";
+import './index.scss';
+import profileImage from '../../assets/images/home/profileImage.png';
 
 export const Home = () => {
   return (
     <section className="hero-section">
+      <div className="circle circle-large"></div>
+      <div className="circle circle-small"></div>
+
       <div className="text-content">
         <p className="role">UI/UX Design and QA Tester</p>
         <h1 className="name">FIKRI ZAKKA</h1>
       </div>
 
       <div className="image-content">
-        <div className="circle circle-large"></div>
-        <div className="circle circle-small"></div>
-
         <img
           src={profileImage}
           alt="Fikri Zakka Profile"
@@ -22,11 +22,11 @@ export const Home = () => {
       </div>
 
       <div className="quote-content">
-        <span className="quote-mark">“</span>
+        <p className="quote-mark">“</p>
         <p className="summary-text">
-          I&apos;m a dedicated and results-driven UI/UX designer and tester with a
-          Bachelor's degree in Information Systems from Universitas Pembangunan
-          Nasional Veteran Jakarta.
+          I&apos;m a dedicated and results-driven UI/UX designer and tester with
+          a Bachelor's degree in Information Systems from Universitas
+          Pembangunan Nasional Veteran Jakarta.
         </p>
       </div>
     </section>
